cb1facb9acfa7ba7655806b29d8d34a51ac8d51f
GitHub Action <action@github.com>
2021-09-20 07:54:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/wshzy/markdown/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wshzy/markdown/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wshzy.markdown.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wshzy.markdown.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
